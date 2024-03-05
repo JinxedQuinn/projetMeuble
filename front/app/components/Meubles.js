c026018ce1daac43f17c89a88d6f0cffda85e7c0
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-05 16:33:23 +0100

    Front - Construction Meubles Page Accueil

--- front/app/components/Meubles.js
+++ front/app/components/Meubles.js
@@ -3,15 +3,20 @@ import Link from 'next/link';
 export default function gridMeubles() {
     return (
         <grid>
-            <div className='grid grid-cols-3 gap-4'>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
+            <div className='grid grid-cols-3 gap-4 m-3'>
+                <div>
+                    <Link href='/meubles/1'>
+                        <div className='border border-red-600'>
+                            <div>
+                                Blahaj
+                                Requin Bleu
+                            </div>
+                        </div>
+                    </Link>
+                    <button>
+                        Ajout au panier
+                    </button>
+                </div>
             </div>
         </grid>
     )
