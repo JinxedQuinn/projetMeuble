846a7fe63746ce15a8524a10691e51c32bf2f47c
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-03-11 16:45:45 +0100

    A faire : afficher une page avec les détails du produit en cliquant

--- front/app/components/Meubles.js
+++ front/app/components/Meubles.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
+let URL = 'http://localhost:3000/api/stuff/65e9c6397c0612b5642acedf';
+
+function getMeubles() {
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+
 
 export default function gridMeubles() {
     return (
         <grid>
             <div className='grid grid-cols-3 gap-4 m-3'>
                 <div>
-                    <Link href='/'>
-                        <div className='border border-red-600 grid grid-cols-2 p-2'>
-                            <div>
-                                <div className='text-center'>
-                                    <p className='font-bold'>BLÅHAJ</p>
-                                    <p>Peluche, requin</p>
-                                    <p>100% Polyester</p>
-                                </div>
-                            </div>
-                            <div className='pl-1 ml-1'>
-                                <img src="/blahaj.png" alt="Blahaj" />
-                            </div>
-                        </div>
-                    </Link>
-                    <a className="inline-flex" href>
-                        <span className="h-12 flex items-center justify-center uppercase font-semibold px-8 border border-black hover:bg-black hover:text-white transition duration-500 ease-in-out">Ajouter au panier</span>
-                        <span className="h-12 w-12 flex-shrink-0 flex items-center justify-center border border-l-0 border-black hover:bg-black hover:text-white transition duration-500 ease-in-out">
-                            <svg className="h-3 w-3" aria-hidden="true" focusable="false" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" class="svg-inline--fa fa-chevron-right fa-w-8 fa-9x"><path fill="currentColor" d="M24.707 38.101L4.908 57.899c-4.686 4.686-4.686 12.284 0 16.971L185.607 256 4.908 437.13c-4.686 4.686-4.686 12.284 0 16.971L24.707 473.9c4.686 4.686 12.284 4.686 16.971 0l209.414-209.414c4.686-4.686 4.686-12.284 0-16.971L41.678 38.101c-4.687-4.687-12.285-4.687-16.971 0z" /></svg>
-                        </span>
-                    </a>
-                </div>
-                <div>
-                    <Link href='/'>
+                    <Link href='about:blank'>
                         <div className='border border-red-600 grid grid-cols-2 p-2'>
                             <div>
                                 <div className='text-center'>
